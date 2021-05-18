dacc347e7adc12041ce392e8451df9c1756cd2e5
Gomolemo Bojosi <gee@geebojosi.com>
2021-05-18 10:21:27 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack', ' ']);
-
+  const img = document.createElement('img');
+  element.classList.add('element')
+  img.setAttribute('src', './UI_Me.jpg');
+  console.log(img)
+// lodash now included locally in the script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(img)
   return element;
 }
 
