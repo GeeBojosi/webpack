b98d2bcfac211fe0a213370557c5a6b61c5ed306
Gomolemo Bojosi <gee@geebojosi.com>
2021-05-19 08:13:21 +0200

    asdjuwaihc

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ function component() {
   const element = document.createElement('div');
   const img = document.createElement('img');
   element.classList.add('element')
-  img.setAttribute('src', './lidye-1Shk_PkNkNw-unsplash.jpg');
+  img.setAttribute('src', './imgs/lidye-1Shk_PkNkNw-unsplash-min.jpg');
+  img.style.width = "2.5rem"
   console.log(img)
 // lodash now included locally in the script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.appendChild(img)
+  element.appendChild(img);
   return element;
 }
 
